f3e8d2202e995c7db9e6b8b68748dda07927834b
rahulgautam137 <rahulgautam137@gmail.com>
2019-12-16 16:35:16 +0530

    updates done

--- src/main/java/restassuredapitesting/FirstAPITest.java
+++ src/main/java/restassuredapitesting/FirstAPITest.java
@@ -1,4 +1,14 @@
 package main.java.restassuredapitesting;
 
+import org.testng.annotations.BeforeTest;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static  org.hamcrest.Matchers.*;
 public class FirstAPITest {
+
+    @BeforeTest
+    public void firstFunction(){
+
+
+    }
 }
