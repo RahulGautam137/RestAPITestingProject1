86c47e734451d6c65eccad552c0817dd49817eea
rahulgautam137 <rahulgautam137@gmail.com>
2020-02-03 10:07:12 +0530

    adding testng files

--- src/main/java/restassuredapitesting/FirstAPITest.java
+++ src/main/java/restassuredapitesting/FirstAPITest.java
@@ -2,12 +2,14 @@ package main.java.restassuredapitesting;
 
 import io.restassured.response.Response;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static  org.hamcrest.Matchers.*;
 public class FirstAPITest {
 
-    @BeforeTest
+    @Test
     public void firstFunction(){
         //"http://api.openweathermap.org/data/2.5/weather?q=London&appid=dc735b6de1394239a29c68b35248369d"
         Response resp=given().param("q","London").param("appid","dc735b6de1394239a29c68b35248369d").when().get("http://api.openweathermap.org/data/2.5/weather");
