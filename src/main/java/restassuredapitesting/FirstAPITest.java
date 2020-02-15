9588bff9c537a3a8301b1ed07794744a73614af7
RahulGautam137 <rahulgautam137@gmail.com>
2020-02-15 12:54:44 +0530

    new changes

--- src/main/java/restassuredapitesting/FirstAPITest.java
+++ src/main/java/restassuredapitesting/FirstAPITest.java
@@ -12,13 +12,31 @@ public class FirstAPITest {
     @Test
     public void firstFunction(){
         //"http://api.openweathermap.org/data/2.5/weather?q=London&appid=dc735b6de1394239a29c68b35248369d"
-        Response resp=given().param("q","London").param("appid","dc735b6de1394239a29c68b35248369d").when().get("http://api.openweathermap.org/data/2.5/weather");
 
+
+                given().
+                param("q","London").
+                param("appid","dc735b6de1394239a29c68b35248369d").
+                when().get("http://api.openweathermap.org/data/2.5/weather").
+                        then().assertThat().statusCode(200);
+
+
+
+
+    }
+
+    @Test
+    public void Test2(){
+        Response resp=given().
+                param("q","London").
+                param("appid","dc735b6de1394239a29c68b35248369d").
+                when().
+                get("http://api.openweathermap.org/data/2.5/weather");
+        System.out.println(resp.asString());
         System.out.println(resp.getStatusCode());
         System.out.println(resp.getBody());
         System.out.println(resp.getStatusCode());
         System.out.println(resp.getBody());
-
 
     }
 }
