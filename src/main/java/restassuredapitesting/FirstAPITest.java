35c781efd54c75828520b9976269dde02ae8a731
RahulGautam137 <rahulgautam137@gmail.com>
2020-02-16 03:34:56 +0530

    new changes

--- src/main/java/restassuredapitesting/FirstAPITest.java
+++ src/main/java/restassuredapitesting/FirstAPITest.java
@@ -43,11 +43,30 @@ public class FirstAPITest {
     @Test
     public void Test3(){
         String description=given().
-                param("zip","110032").
+                param("zip","110032,IN").
                 param("appid","dc735b6de1394239a29c68b35248369d").
                 when().
                 get("http://api.openweathermap.org/data/2.5/weather").
-                then().contentType(ContentType.JSON).extract().path("$.weather..description");
+                then().contentType(ContentType.JSON).extract().path("weather[0].description");
+
+
+    }
+    @Test
+    public void getDataAndPassToAPI(){
+
+        Response resp1=given().
+                param("appid","dc735b6de1394239a29c68b35248369d").
+                param("zip","110032,IN").when().get("http://api.openweathermap.org/data/2.5/weather");
+        System.out.println(resp1.asString());
+        Float longitude=resp1.
+                then().contentType(ContentType.JSON).extract().path("coord.lon");
+        System.out.println("longtitute value "+longitude);
+        Float latitude=resp1.
+                then().contentType(ContentType.JSON).extract().path("coord.lat");
+        System.out.println("latitude value "+latitude);
+        Response resp= given().param("lon", longitude).param("lat", latitude).param("appid","dc735b6de1394239a29c68b35248369d").
+                when().get("http://api.openweathermap.org/data/2.5/weather");
+        System.out.println("Response code is "+resp.asString());
 
     }
 }
